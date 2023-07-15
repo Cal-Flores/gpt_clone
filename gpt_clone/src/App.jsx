da1f73d1c6982b095b207d80f5494898141eb79b
calebflores <calebflores1388@gmail.com>
2023-07-15 18:08:03 -0500

    generate image err

--- gpt_clone/src/App.jsx
+++ gpt_clone/src/App.jsx
@@ -1,17 +1,26 @@
 import { useState } from 'react'
-import {Configuration, OpenAIApi} from "openai"
+import { Configuration, OpenAIApi } from "openai"
 import './App.css'
 
 function App() {
   const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-});
+    apiKey: import.meta.env.VITE_Open_AI_Key,
+  });
 
+  const openai = new OpenAIApi(configuration)
+  const generateImage = async () => {
+    const res = await openai.createImage({
+      prompt: "Chocolate lab",
+      n: 1,
+      size: "1024x1024"
+    })
+    console.log(res.data.data[0].url)
+  }
   return (
 
-      <div>
-
-      </div>
+    <div>
+      <button onClick={generateImage}>Generate an Image!</button>
+    </div>
 
   )
 }
